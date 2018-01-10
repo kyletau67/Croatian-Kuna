8fff6d2a9a7274b55456e9946f8bf8cdaeaafd14
rlin6 <rlin6@stuy.edu>
2018-01-10 09:20:09 -0500

    Update Card and Created Player

--- Card.java
+++ Card.java
@@ -1,13 +1,14 @@
 import java.util.Map;
 import java.util.HashMap;
 
-public class Card implements Cardkeys{
+public class Card {
 
     public String suit;
     public int val;
     
     public Card(int key) {
-	String str = Card.get(key);
+	Cardkeys k = new Cardkeys();
+	String str = k.get(key);
 	suit = str.substring(str.length()-1);
 	if (key % 13 == 9) {
 	    val = 10;
---
+++ Player.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class Player {
+    public String name;
+    public double balance;
+    private ArrayList<Comparable> hand;
+    public Player() {
+	//balance = 10000;
+	//
+    
